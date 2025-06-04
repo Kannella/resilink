0edb1de7e5453ef71b92066d14705da49f586748
Giovanni <gi.canella85@outlook.com>
2025-06-04 20:39:15 -0300

    eonet api map controller implemented

--- demo/src/main/java/com/reslink/demo/DemoApplication.java
+++ demo/src/main/java/com/reslink/demo/DemoApplication.java
@@ -3,11 +3,9 @@ package com.reslink.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.reslink.demo")
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
--- demo/src/main/java/com/reslink/demo/controller/EventoController.java
+++ demo/src/main/java/com/reslink/demo/controller/EventoController.java
@@ -1,13 +1,13 @@
 package com.reslink.demo.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+
 import com.reslink.demo.dto.EventoDTO;
 import com.reslink.demo.service.EonetService;
-import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +18,13 @@ public class EventoController {
     @Autowired
     private EonetService eonetService;
 
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+
     @GetMapping("/ativos")
-    public ResponseEntity<List<EventoDTO>> listarEventos() throws JSONException {
+    public ResponseEntity<List<EventoDTO>> listarEventos() {
         return ResponseEntity.ok(eonetService.buscarEventosAtivos());
     }
 }
